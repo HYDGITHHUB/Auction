a612de3653d298c41f2f53fb5c7a238a25b723ab
HYDGITHHUB <2607223301@qq.com>
2019-12-16 10:13:37 +0800

    debug

--- src/servlet/selectServlet.java
+++ src/servlet/selectServlet.java
@@ -14,27 +14,52 @@ import entity.Booking;
 
 public class selectServlet extends HttpServlet {
 
+	/**
+	 * Constructor of the object.
+	 */
 	public selectServlet() {
 		super();
 	}
 
+	/**
+	 * Destruction of the servlet. <br>
+	 */
 	public void destroy() {
 		super.destroy(); // Just puts "destroy" string in log
 		// Put your code here
 	}
 
-
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		doPost(request, response);
 	}
 
-
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
@@ -55,7 +80,11 @@ public class selectServlet extends HttpServlet {
 		out.close();
 	}
 
-
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
 	public void init() throws ServletException {
 		// Put your code here
 	}
